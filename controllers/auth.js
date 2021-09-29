2836c424c70287d923277842b890a7d93463ce5e
politamorocho <Tu correopolitamorocho@gmail.com>
2021-09-29 11:57:16 -0500

    google sign añadido

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,8 @@ const {response} = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario=require('../models/usuario');
 const { generarJWT } = require('../helpers/generarJWT');
-require
+const { googleVerify } = require('../helpers/google-verify');
+
 
 
 const login= async(req, res=response)=>{
@@ -54,6 +55,61 @@ const login= async(req, res=response)=>{
     
 }
 
+const googleSignIn = async (req, res=response)=>{
+ const {id_token} = req.body;
+
+ try {
+     const {nombre, img, correo} = await googleVerify(id_token);
+    console.log({nombre, img, correo});
+    
+   let usuario = await Usuario.findOne({correo});
+
+    if(!usuario){
+        //si no existe hay que crearlo
+        const data={
+            nombre,
+            correo,
+            password: ':P',
+            img,
+            google:true,
+            
+
+        };
+        usuario= new Usuario(data);
+        await usuario.save();
+    }
+
+    //estado del usuario en bd
+    if(!usuario.estado){
+       return res.status(401).json({
+            msg: 'hable con el admin, usted no existe'
+        })
+    }
+
+   // generar jwt
+    const token = await generarJWT(usuario.id);
+
+
+
+    res.json({
+        usuario,
+        token
+    })  
+
+ } catch (error) {
+    res.status(400).json({
+        ok: false,
+        msg: 'el token no se puede verificar'
+    })
+
+     
+ }
+
+ 
+}
+
+
 module.exports={
-    login
+    login,
+    googleSignIn
 }
